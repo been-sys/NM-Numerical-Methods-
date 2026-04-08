5eee436de598e713cb55634c37e4eb6f3e770ea6
been-sys <volhinbogdan1@gmail.com.com>
2026-04-08 19:22:47 +0700

    check

--- ПЗ3/pz3/main.py
+++ ПЗ3/pz3/main.py
@@ -6,9 +6,9 @@ def main():
     # M.data = [[2, 0, 1], [0, 2, 0], [1, 0, 2]]
     M.data = [[24.92, 26], [26, 31.33]]
 
-    H, Q = M.Hessenberg(EPS=0.00001)
+    H, Q = M.Hessenberg(EPS=0.01)
 
-    eval, evec = H.QR_iterations(EPS=0.00001)
+    eval, evec = H.QR_iterations(EPS=0.01)
 
     print("Eigen values:")
     print(*eval)
